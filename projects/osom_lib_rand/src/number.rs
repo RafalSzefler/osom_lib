8f7fc19a1aa62e664a1e6f053cabd09b9e503e6d
Rafal Szefler <rafael.szefler@gmail.com>
2025-07-06 22:50:03 +0200

    Fixed docs

--- projects/osom_lib_rand/src/number.rs
+++ projects/osom_lib_rand/src/number.rs
@@ -18,34 +18,69 @@ trait Private {}
 pub trait Number:
     Clone + Copy + Debug + Display + PartialEq + Eq + Hash + PartialOrd + Ord + Default + Private
 {
+    /// Represents the associated byte representation of the number, e.g. `[u8; 4]` for `u32`.
+    type ByteRepr: AsRef<[u8]> + AsMut<[u8]>;
+
+    /// Returns the size of the number in bytes.
     #[must_use]
-    fn size() -> usize;
+    fn size() -> usize {
+        size_of::<Self>()
+    }
+
+    /// Returns the sum of two numbers, wrapping around if the result is too large.
     #[must_use]
     fn wrapping_add(self, other: Self) -> Self;
+
+    /// Returns the difference of two numbers, wrapping around if the result is too small.
     #[must_use]
     fn wrapping_sub(self, other: Self) -> Self;
+
+    /// Returns the product of two numbers, wrapping around if the result is too large.
     #[must_use]
     fn wrapping_mul(self, other: Self) -> Self;
+
+    /// Returns the quotient of two numbers, wrapping around if the result is too large.
     #[must_use]
     fn wrapping_div(self, other: Self) -> Self;
+
+    /// Returns the remainder of the division of two numbers.
     #[must_use]
     fn wrapping_rem(self, other: Self) -> Self;
+
+    /// Returns the result of the left shift of the number by the given number of bits.
     #[must_use]
     fn wrapping_shl(self, other: u32) -> Self;
+
+    /// Returns the result of the right shift of the number by the given number of bits.
     #[must_use]
     fn wrapping_shr(self, other: u32) -> Self;
+
+    /// Returns the number one.
     #[must_use]
     fn one() -> Self;
+
+    /// Returns the number zero.
     #[must_use]
     fn zero() -> Self {
         Self::default()
     }
+
+    /// Returns the highest possible value of the number.
     #[must_use]
     fn highest() -> Self;
+
+    /// Returns the number as a `u128`, which is guaranteed to be big enough to hold
+    /// any [`Number`] value.
     #[must_use]
     fn as_u128(self) -> u128;
+
+    /// Creates a number from its little-endian representation in bytes.
     #[must_use]
     fn from_le_bytes(bytes: &[u8]) -> Self;
+
+    /// Returns the little-endian representation of the number in bytes.
+    #[must_use]
+    fn to_le_bytes(self) -> Self::ByteRepr;
 }
 
 impl Private for u32 {}
@@ -53,9 +88,8 @@ impl Private for u64 {}
 impl Private for u128 {}
 
 impl Number for u32 {
-    fn size() -> usize {
-        size_of::<Self>()
-    }
+    type ByteRepr = [u8; 4];
+
     fn one() -> Self {
         1
     }
@@ -92,12 +126,14 @@ impl Number for u32 {
         result.copy_from_slice(bytes);
         Self::from_le_bytes(result)
     }
+    fn to_le_bytes(self) -> Self::ByteRepr {
+        self.to_le_bytes()
+    }
 }
 
 impl Number for u64 {
-    fn size() -> usize {
-        size_of::<Self>()
-    }
+    type ByteRepr = [u8; 8];
+
     fn one() -> Self {
         1
     }
@@ -134,12 +170,14 @@ impl Number for u64 {
         result.copy_from_slice(bytes);
         Self::from_le_bytes(result)
     }
+    fn to_le_bytes(self) -> Self::ByteRepr {
+        self.to_le_bytes()
+    }
 }
 
 impl Number for u128 {
-    fn size() -> usize {
-        size_of::<Self>()
-    }
+    type ByteRepr = [u8; 16];
+
     fn one() -> Self {
         1
     }
@@ -175,6 +213,9 @@ impl Number for u128 {
         let mut result = [0u8; size_of::<Self>()];
         result.copy_from_slice(bytes);
         Self::from_le_bytes(result)
+    }
+    fn to_le_bytes(self) -> Self::ByteRepr {
+        self.to_le_bytes()
     }
 }
 
